f8ea7349f4a3ddb9c7cb9e88a3062a2496f50ce4
dessert-developer <jsm5656@gmail.com>
2021-04-01 17:42:06 +0900

    batch 파일
    대표님의 지시를 수행하기 위한 파일

---
+++ update.bat
@@ -0,0 +1,12 @@
+@echo off
+for /f "tokens=2 delims==" %%a in ('type "comport.txt" ^|findstr /bi "COM="') do (
+    set "str=%%a" & goto :continue
+)
+:continue
+echo "%str%"
+
+esptool.exe --chip esp32 --port COM"%str%" --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_qio_80m.bin 0x10000 popsign.ino.bin 0x8000 popsign.ino.partitions.bin
+
+
+pause
+ 
