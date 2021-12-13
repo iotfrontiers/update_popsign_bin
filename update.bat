d51efcc4c549114fb8ed2326e1720e83c3237fd0
greetdev <fjiogweh@gmail.com>
2021-12-13 18:03:19 +0900

    erase port 추가

--- update.bat
+++ update.bat
@@ -1,5 +1,4 @@
 @echo off
-
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\
 :: [전체 설명]
 :: 컴퓨터에 연결된 포트들 중에서 팝사인이 연결된 포트 번호를 가져와서
@@ -11,7 +10,9 @@
 :: 그 중에서 CH340을 포함한 포트 이름만 추출
 set "port_number=0"
 for /f "tokens=1* delims==" %%I in ('wmic path win32_pnpentity get caption /format:list ^| find "CH340"') do (
+    echo %%~J
     call :setCOM "%%~J" & goto :continue
+    
 )
 
 :: :setCOM <WMIC_output_line>
@@ -23,9 +24,8 @@ set "str=%~1"
 set "num=%str:*(COM=%"
 set "num=%num:)=%"
 set "port_number=%num%"
-
-:: 바이너리 쓰는 부분
-esptool.exe erase_region 0xc90000 0x370000
+echo %port_number%
+esptool.exe --chip esp32 --port COM"%port_number%" erase_region 0xc90000 0x370000 
 esptool.exe --chip esp32 --port COM"%port_number%" --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_qio_80m.bin 0x10000 popsign.ino.bin 0x8000 popsign.ino.partitions.bin
 endlocal
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
