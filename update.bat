73054fbe305ef74747b2372071e83a87c6cfbbd3
D <rehwieh@gmail.com>
2022-03-15 10:51:34 +0900

    삭제 범위 변경

--- update.bat
+++ update.bat
@@ -12,7 +12,7 @@ set "str=%~1"
 set "num=%str:*(COM=%"
 set "num=%num:)=%"
 set "port_number=%num%"
-esptool.exe --chip esp32 --port COM"%port_number%" erase_region 0x9000 0x1000000 
+esptool.exe --chip esp32 --port COM"%port_number%" erase_region 0x9000 0xFF7000
 esptool.exe --chip esp32 --port COM"%port_number%" --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 boot_app0.bin 0x1000 bootloader_qio_80m.bin 0x10000 popsign.ino.bin 0x8000 popsign.ino.partitions.bin
 endlocal
 
